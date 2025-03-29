6c3fae8ac64ea9e46be2166eabc78278d5c41cc1
sharyar-dev <onshariiit@gmail.com>
2025-03-29 14:50:45 +0500

    add AppExtensions component to display extensions list and update App.vue layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import AppHeader from "@/components/AppHeader.vue";
+import AppExtensions from "@/components/AppExtensions.vue";
 </script>
 
 <template>
   <div class="min-h-svh w-full bg-[linear-gradient(180deg,#EBF2FC,#EEFBF9)] dark:bg-[linear-gradient(180deg,#04091B,#091540)] px-4 py-5 md:px-8 md:py-6">
     <div class="lg:max-w-[1170px] mx-auto">
-      <AppHeader />
+      <AppHeader class="mb-500 lg:mb-800" />
+      <AppExtensions />
     </div>
   </div>
 </template>
---
+++ src/components/AppExtensions.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { shallowRef } from "vue";
+import AppTabs from "@/components/AppTabs.vue";
+import AppTab from "@/components/AppTab.vue";
+import AppCard from "@/components/AppCard.vue";
+
+import data from "@/data.json";
+import AppButton from "@/components/AppButton.vue";
+import AppToggle from "@/components/AppToggle.vue";
+
+const extensionsList = shallowRef(data);
+</script>
+
+<template>
+  <div class="flex flex-col gap-400">
+    <div class="flex flex-col items-center md:flex-row md:justify-between gap-300">
+      <h1 class="text-preset-1 text-neutral-900 dark:text-neutral-0">Extensions List</h1>
+      <AppTabs>
+        <AppTab :is-active="true">All</AppTab>
+        <AppTab>Active</AppTab>
+        <AppTab>Inactive</AppTab>
+      </AppTabs>
+    </div>
+    <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-150">
+      <AppCard v-for="extension in extensionsList" :key="extension.name">
+        <div class="flex flex-col gap-300">
+          <div class="flex items-start gap-200">
+            <img :src="extension.logo" :alt="extension.name" width="60px" height="60px" />
+            <div class="flex flex-col gap-100">
+              <h3 class="text-preset-2 text-neutral-900 dark:text-neutral-0">{{ extension.name }}</h3>
+              <p class="text-preset-5 text-neutral-600 dark:text-neutral-300">{{ extension.description }}</p>
+            </div>
+          </div>
+          <div class="flex items-center justify-between">
+            <AppButton>Remove</AppButton>
+            <AppToggle :checked="extension.isActive" />
+          </div>
+        </div>
+      </AppCard>
+    </div>
+  </div>
+</template>
