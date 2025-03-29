12a10c02aa707d3bbb0752f2e585dd7287981859
sharyar-dev <onshariiit@gmail.com>
2025-03-29 13:47:08 +0500

    add AppHeader and AppLogo components, implement dark mode toggle, and update App.vue layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppHeader from "@/components/AppHeader.vue";
+</script>
 
 <template>
-  <h1 class="text-preset-1">App</h1>
+  <div class="min-h-svh w-full bg-[linear-gradient(180deg,#EBF2FC,#EEFBF9)] dark:bg-[linear-gradient(180deg,#04091B,#091540)] px-4 py-5 md:px-8 md:py-6">
+    <div class="lg:max-w-[1170px] mx-auto">
+      <AppHeader />
+    </div>
+  </div>
 </template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { useDark, useToggle } from '@vueuse/core';
+import AppLogo from "@/components/AppLogo.vue";
+
+const isDark = useDark({
+  selector: 'html',
+  attribute: 'class',
+  valueDark: 'theme-dark',
+  valueLight: '',
+});
+
+const toggleDark = useToggle(isDark);
+
+const themeIcon = computed(() => {
+  return isDark.value ? '/icons/icon-sun.svg' : '/icons/icon-moon.svg';
+});
+
+const themeText = computed(() => {
+  return isDark.value ? 'Change to Light Mode' : 'Change to Dark Mode';
+});
+</script>
+
+<template>
+  <div class="bg-neutral-0 dark:bg-neutral-800 py-2 px-3 md:px-4 md:py-3 lg:py-3 lg:px-4 rounded-20 flex items-center justify-between border border-neutral-200 dark:border-none shadow-xs">
+    <AppLogo class="text-neutral-900 dark:text-white" />
+    <button class="rounded-12 bg-neutral-100 dark:bg-neutral-700 p-3.5" @click="toggleDark()">
+      <img :src="themeIcon" :alt="themeText" />
+    </button>
+  </div>
+</template>
