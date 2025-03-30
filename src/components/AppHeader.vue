761d439e4a0d38a222f10fa7a01e27d2b63c5d0e
sharyar-dev <onshariiit@gmail.com>
2025-03-30 09:54:48 +0500

    fix: replace div with header element for improved semantic structure in AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -22,10 +22,10 @@ const themeText = computed(() => {
 </script>
 
 <template>
-  <div class="bg-neutral-0 dark:bg-neutral-800 py-2 px-3 md:px-4 md:py-3 lg:py-3 lg:px-4 rounded-20 flex items-center justify-between border border-neutral-200 dark:border-none shadow-xs">
+  <header class="bg-neutral-0 dark:bg-neutral-800 py-2 px-3 md:px-4 md:py-3 lg:py-3 lg:px-4 rounded-20 flex items-center justify-between border border-neutral-200 dark:border-none shadow-xs">
     <AppLogo class="text-neutral-900 dark:text-white" />
     <button class="rounded-12 bg-neutral-100 dark:bg-neutral-700 p-3.5" @click="toggleDark()">
       <img :src="themeIcon" :alt="themeText" />
     </button>
-  </div>
+  </header>
 </template>
