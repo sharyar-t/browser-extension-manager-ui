f8cf66783c80eb3ef0e6397ca56872e77c8c1afd
sharyar-dev <onshariiit@gmail.com>
2025-03-30 09:37:41 +0500

    enhance AppToggle component with improved focus styles and accessibility features

--- src/components/AppToggle.vue
+++ src/components/AppToggle.vue
@@ -10,6 +10,6 @@ defineEmits(['change'])
 <template>
   <label class="inline-flex items-center cursor-pointer">
     <input type="checkbox" :checked="checked" class="sr-only peer" @change="$emit('change')">
-    <div class="relative w-450 h-250 bg-neutral-400 rounded-full peer dark:bg-neutral-600 peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-neutral-0 after:rounded-full after:h-200 after:w-200 after:transition-all peer-checked:bg-red-700 dark:peer-checked:bg-red-400"></div>
+    <span class="relative w-450 h-250 bg-neutral-400 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-red-400 dark:peer-focus:ring-red-700 rounded-full peer dark:bg-neutral-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-neutral-0 after:rounded-full after:h-200 after:w-200 after:transition-all peer-checked:bg-red-700 dark:peer-checked:bg-red-400"></span>
   </label>
 </template>
